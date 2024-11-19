ae6f18cab7197473b42f3a52fdb3444c728db130
Etsubitu <etsubitu@gmail.com>
2024-11-18 17:38:11 -0800

    additional rows

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -4,10 +4,10 @@ const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/ movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/ movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/ movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumenatries: `/discover/ movie?api_key=${API_KEY}&with_genres=99`,
-  fetchTvshow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumenatries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 export default requests;
